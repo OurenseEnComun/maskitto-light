a6ccdedf61a16b1c1619a2195b6ea8a2cfde8ce0
Emilio J. Padron <emilioj@udc.es>
2015-10-06 18:56:44 +0200

    Engadido MIME type para epub

--- functions.php
+++ functions.php
@@ -14,3 +14,9 @@ require get_template_directory() . '/inc/initialize.php';
 if ( ! isset($content_width)) {
     $content_width = 980;
 }
+
+function my_myme_types($mime_types){
+    $mime_types['epub'] = 'application/zip'; //Adding epub extension
+    return $mime_types;
+}
+add_filter('upload_mimes', 'my_myme_types', 1, 1);
